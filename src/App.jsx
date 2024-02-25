ecd1610aac282cebaf9b0c306b541f2c4fec8e3f
Amanda Chawinska <amanda.chawinska@spoko.pl>
2024-02-25 15:45:57 +0100

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import { Navbar } from "./components/Navbar/Navbar";
+
 function App() {
-  return <div className="container"> LALALALl</div>;
+  return (
+    <div className="container">
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,44 @@
+import { getImageUrl } from "../../utils";
+import { useState } from "react";
+import styles from "./Navbar.module.css";
+
+export const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  return (
+    <nav className="pt-15 relative z-[2] flex flex-col items-start justify-between lg:flex-row lg:items-center">
+      <a className="text-3xl font-semibold" href="/">
+        Portfolio
+      </a>
+      <div className={styles.menu}>
+        <img
+          className={styles.menuBtn}
+          src={
+            menuOpen
+              ? getImageUrl("nav/closeIcon.png")
+              : getImageUrl("nav/menuIcon.png")
+          }
+          alt="menu-button"
+          onClick={() => setMenuOpen(!menuOpen)}
+        />
+        <ul
+          className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`}
+          onClick={() => setMenuOpen(false)}
+        >
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#experience">Experience</a>
+          </li>
+          <li>
+            <a href="#projects">Projects</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
